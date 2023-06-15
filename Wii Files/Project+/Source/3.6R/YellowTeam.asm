d4df793465761041a83bbe7d6fcd5031005649d5
ilikepizza107 <90655367+ilikepizza107@users.noreply.github.com>
2023-06-14 21:54:28 -0500

    More Yellow Team stuff
    Everything EXCEPT shield colors and death plumes, as well as Results Screen colors, have been corrected to be Yellow
    Of note: I found a bug in vBrawl where the CPU's magnifying glass in team battle would ALWAYS use the desaturated red. I changed this so they use their respective team colors now

--- Wii Files/Project+/Source/3.6R/YellowTeam.asm
+++ Wii Files/Project+/Source/3.6R/YellowTeam.asm
@@ -1,7 +1,11 @@
 ####################################################
 Green Team is Yellow Team [ilikepizza107, QuickLava]
 ####################################################
-word 0x00010200 @ $806A0EAC   # UI Elements, excluding the hand
-word 0x01020300 @ $806A1004   # hand
 word 0x00010200 @ $805A21F0   # search for Yellow costumes, not Green ones!
-word 0x00010200 @ $806A0DA8   # choose yellow costumes!
+word 0x00010200 @ $806A0DA8   # choose yellow costumes in-game!
+word 0x00010200 @ $806A0EAC   # CLR0 UI Elements, excluding the hand
+word 0x00010200 @ $806A0EE0   # "?" texture on the CSS for Random uses Yellow, not Green!
+word 0x01020300 @ $806A1004   # hand
+word 0x02070000 @ $805A23AC   # in-battle UI (requires modified CLR0 colors in info.pac, specifrically InfArrow_TopN_0, InfFace_TopN__0, InfMark_TopN__0, and InfPlynm_TopN__0)
+
+
