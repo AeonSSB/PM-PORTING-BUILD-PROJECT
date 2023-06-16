7c094843779cf2caf9091bbfce0cc44184efb544
ilikepizza107 <90655367+ilikepizza107@users.noreply.github.com>
2023-06-15 20:30:24 -0500

    Yellow shields and death plumes
    Just need results screen to use yellow and we're done

--- Wii Files/Project+/Source/3.6R/YellowTeam.asm
+++ Wii Files/Project+/Source/3.6R/YellowTeam.asm
@@ -7,5 +7,6 @@ word 0x00010200 @ $806A0EAC   # CLR0 UI Elements, excluding the hand
 word 0x00010200 @ $806A0EE0   # "?" texture on the CSS for Random uses Yellow, not Green!
 word 0x01020300 @ $806A1004   # hand
 word 0x02070000 @ $805A23AC   # in-battle UI (requires modified CLR0 colors in info.pac, specifrically InfArrow_TopN_0, InfFace_TopN__0, InfMark_TopN__0, and InfPlynm_TopN__0)
-
+op mr r4, r23 @ $8081DEEC     # \
+op mr r4, r23 @ $8081DEF4     # / Yellow shields and death plumes
 
