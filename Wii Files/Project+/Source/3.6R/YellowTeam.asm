5a6366a9ea243b25214fbde8ea23f86e3034b081
ilikepizza107 <90655367+ilikepizza107@users.noreply.github.com>
2023-06-15 20:31:34 -0500

    Update YellowTeam.asm
    Added helpful comment

--- Wii Files/Project+/Source/3.6R/YellowTeam.asm
+++ Wii Files/Project+/Source/3.6R/YellowTeam.asm
@@ -1,12 +1,15 @@
 ####################################################
 Green Team is Yellow Team [ilikepizza107, QuickLava]
+
+Requires Team Coloured Shields V2 from RSBE01 to 
+be removed
 ####################################################
 word 0x00010200 @ $805A21F0   # search for Yellow costumes, not Green ones!
 word 0x00010200 @ $806A0DA8   # choose yellow costumes in-game!
 word 0x00010200 @ $806A0EAC   # CLR0 UI Elements, excluding the hand
 word 0x00010200 @ $806A0EE0   # "?" texture on the CSS for Random uses Yellow, not Green!
 word 0x01020300 @ $806A1004   # hand
-word 0x02070000 @ $805A23AC   # in-battle UI (requires modified CLR0 colors in info.pac, specifrically InfArrow_TopN_0, InfFace_TopN__0, InfMark_TopN__0, and InfPlynm_TopN__0)
+word 0x02070000 @ $805A23AC   # in-battle UI (requires modified CLR0 colors in info.pac, specifically InfArrow_TopN_0, InfFace_TopN__0, InfMark_TopN__0, and InfPlynm_TopN__0)
 op mr r4, r23 @ $8081DEEC     # \
 op mr r4, r23 @ $8081DEF4     # / Yellow shields and death plumes
 
