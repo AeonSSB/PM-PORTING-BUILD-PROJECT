e80ca7c5cde069bfbc61f66ff84d388ee6fca506
Aeon <efisiomii@gmail.com>
2025-05-22 11:24:16 +0200

    Code Menu update
    The GCTRM not being updated was the reason for the previous issues

--- Dolphin SD/Reload/Source/CM_Addons/AddonAliases.asm
+++ Dolphin SD/Reload/Source/CM_Addons/AddonAliases.asm
@@ -3,31 +3,107 @@
 #################################################
 # Addon "Hitfalling Toggle v1.0.1 [Eon, mawwwk, QuickLava]" Lines
 # Line "Hitfalling Toggle" in "Hitfalling Toggle v1.0.1 [Eon, mawwwk, QuickLava]"
-.alias WIPMHTFL_ONOFF_LOC = 0x804E26F0
+.alias WIPMHTFL_ONOFF_LOC = 0x804E2EA0
 .alias WIPMHTFL_ONOFF_LOC_HI = 0x804E
-.alias WIPMHTFL_ONOFF_LOC_LO = 0x26F0
+.alias WIPMHTFL_ONOFF_LOC_LO = 0x2EA0
+
+# Addon "Mechanics Hub v1.0.0 [QuickLava]" Lines
+# Line "Active Mechanic" in "Mechanics Hub v1.0.0 [QuickLava]"
+.alias MECH_HUB_LINE_001_LOC = 0x804E2EA4
+.alias MECH_HUB_LINE_001_LOC_HI = 0x804E
+.alias MECH_HUB_LINE_001_LOC_LO = 0x2EA4
+# Line "Magic Series Enabled" in "Mechanics Hub v1.0.0 [QuickLava]"
+.alias MECH_HUB_LINE_005_LOC = 0x804E2EA8
+.alias MECH_HUB_LINE_005_LOC_HI = 0x804E
+.alias MECH_HUB_LINE_005_LOC_LO = 0x2EA8
+# Line "Final Smash Meter Enabled" in "Mechanics Hub v1.0.0 [QuickLava]"
+.alias MECH_HUB_LINE_009_LOC = 0x804E2EAC
+.alias MECH_HUB_LINE_009_LOC_HI = 0x804E
+.alias MECH_HUB_LINE_009_LOC_LO = 0x2EAC
+# Line "Focus Attacks Enabled" in "Mechanics Hub v1.0.0 [QuickLava]"
+.alias MECH_HUB_LINE_013_LOC = 0x804E2EB0
+.alias MECH_HUB_LINE_013_LOC_HI = 0x804E
+.alias MECH_HUB_LINE_013_LOC_LO = 0x2EB0
+# Line "Active Mechanic" in "Mechanics Hub v1.0.0 [QuickLava]"
+.alias MECH_HUB_LINE_002_LOC = 0x804E2EB4
+.alias MECH_HUB_LINE_002_LOC_HI = 0x804E
+.alias MECH_HUB_LINE_002_LOC_LO = 0x2EB4
+# Line "Magic Series Enabled" in "Mechanics Hub v1.0.0 [QuickLava]"
+.alias MECH_HUB_LINE_006_LOC = 0x804E2EB8
+.alias MECH_HUB_LINE_006_LOC_HI = 0x804E
+.alias MECH_HUB_LINE_006_LOC_LO = 0x2EB8
+# Line "Final Smash Meter Enabled" in "Mechanics Hub v1.0.0 [QuickLava]"
+.alias MECH_HUB_LINE_010_LOC = 0x804E2EBC
+.alias MECH_HUB_LINE_010_LOC_HI = 0x804E
+.alias MECH_HUB_LINE_010_LOC_LO = 0x2EBC
+# Line "Focus Attacks Enabled" in "Mechanics Hub v1.0.0 [QuickLava]"
+.alias MECH_HUB_LINE_014_LOC = 0x804E2EC0
+.alias MECH_HUB_LINE_014_LOC_HI = 0x804E
+.alias MECH_HUB_LINE_014_LOC_LO = 0x2EC0
+# Line "Active Mechanic" in "Mechanics Hub v1.0.0 [QuickLava]"
+.alias MECH_HUB_LINE_003_LOC = 0x804E2EC4
+.alias MECH_HUB_LINE_003_LOC_HI = 0x804E
+.alias MECH_HUB_LINE_003_LOC_LO = 0x2EC4
+# Line "Magic Series Enabled" in "Mechanics Hub v1.0.0 [QuickLava]"
+.alias MECH_HUB_LINE_007_LOC = 0x804E2EC8
+.alias MECH_HUB_LINE_007_LOC_HI = 0x804E
+.alias MECH_HUB_LINE_007_LOC_LO = 0x2EC8
+# Line "Final Smash Meter Enabled" in "Mechanics Hub v1.0.0 [QuickLava]"
+.alias MECH_HUB_LINE_011_LOC = 0x804E2ECC
+.alias MECH_HUB_LINE_011_LOC_HI = 0x804E
+.alias MECH_HUB_LINE_011_LOC_LO = 0x2ECC
+# Line "Focus Attacks Enabled" in "Mechanics Hub v1.0.0 [QuickLava]"
+.alias MECH_HUB_LINE_015_LOC = 0x804E2ED0
+.alias MECH_HUB_LINE_015_LOC_HI = 0x804E
+.alias MECH_HUB_LINE_015_LOC_LO = 0x2ED0
+# Line "Active Mechanic" in "Mechanics Hub v1.0.0 [QuickLava]"
+.alias MECH_HUB_LINE_004_LOC = 0x804E2ED4
+.alias MECH_HUB_LINE_004_LOC_HI = 0x804E
+.alias MECH_HUB_LINE_004_LOC_LO = 0x2ED4
+# Line "Magic Series Enabled" in "Mechanics Hub v1.0.0 [QuickLava]"
+.alias MECH_HUB_LINE_008_LOC = 0x804E2ED8
+.alias MECH_HUB_LINE_008_LOC_HI = 0x804E
+.alias MECH_HUB_LINE_008_LOC_LO = 0x2ED8
+# Line "Final Smash Meter Enabled" in "Mechanics Hub v1.0.0 [QuickLava]"
+.alias MECH_HUB_LINE_012_LOC = 0x804E2EDC
+.alias MECH_HUB_LINE_012_LOC_HI = 0x804E
+.alias MECH_HUB_LINE_012_LOC_LO = 0x2EDC
+# Line "Focus Attacks Enabled" in "Mechanics Hub v1.0.0 [QuickLava]"
+.alias MECH_HUB_LINE_016_LOC = 0x804E2EE0
+.alias MECH_HUB_LINE_016_LOC_HI = 0x804E
+.alias MECH_HUB_LINE_016_LOC_LO = 0x2EE0
 
 # Addon "Melee-style Camera Freeze on Game Finish v1.0.0 [Eon, QuickLava]" Lines
 # Line "Melee Game Finish Camera Freeze" in "Melee-style Camera Freeze on Game Finish v1.0.0 [Eon, QuickLava]"
-.alias MLEEFREZ_ONOFF_LOC = 0x804E26F4
+.alias MLEEFREZ_ONOFF_LOC = 0x804E2EE4
 .alias MLEEFREZ_ONOFF_LOC_HI = 0x804E
-.alias MLEEFREZ_ONOFF_LOC_LO = 0x26F4
+.alias MLEEFREZ_ONOFF_LOC_LO = 0x2EE4
 
 # Addon "Raycast Debugger Toggle v1.0.1 [Eon, QuickLava]" Lines
 # Line "Raycast Display" in "Raycast Debugger Toggle v1.0.1 [Eon, QuickLava]"
-.alias RAYDEBUG_ONOFF_LOC = 0x804E26F8
+.alias RAYDEBUG_ONOFF_LOC = 0x804E2EE8
 .alias RAYDEBUG_ONOFF_LOC_HI = 0x804E
-.alias RAYDEBUG_ONOFF_LOC_LO = 0x26F8
+.alias RAYDEBUG_ONOFF_LOC_LO = 0x2EE8
 
 # Addon "Parry Taunts v1.1.0" Lines
 # Line "Parry Taunts" in "Parry Taunts v1.1.0"
-.alias PRYTAUNT_ONOFF_LOC = 0x804E26FC
+.alias PRYTAUNT_ONOFF_LOC = 0x804E2EEC
 .alias PRYTAUNT_ONOFF_LOC_HI = 0x804E
-.alias PRYTAUNT_ONOFF_LOC_LO = 0x26FC
+.alias PRYTAUNT_ONOFF_LOC_LO = 0x2EEC
 
 # Addon "Universal Walljumps v1.0.1 [Dantarion, QuickLava]" Lines
 # Line "Universal Walljump Toggle" in "Universal Walljumps v1.0.1 [Dantarion, QuickLava]"
-.alias UNVWLJMP_SETTING_LOC = 0x804E2700
+.alias UNVWLJMP_SETTING_LOC = 0x804E2EF0
 .alias UNVWLJMP_SETTING_LOC_HI = 0x804E
-.alias UNVWLJMP_SETTING_LOC_LO = 0x2700
+.alias UNVWLJMP_SETTING_LOC_LO = 0x2EF0
+
+# Addon "Volume Balance Control v1.0.0 [QuickLava]" Lines
+# Working Memory (0x08 bytes) for "Volume Balance Control v1.0.0 [QuickLava]"
+.alias VOLBLNCE_WORKING_MEM_LOC = 0x804E2EF4
+.alias VOLBLNCE_WORKING_MEM_LOC_HI = 0x804E
+.alias VOLBLNCE_WORKING_MEM_LOC_LO = 0x2EF4
+# Line "Volume Ratio" in "Volume Balance Control v1.0.0 [QuickLava]"
+.alias VOLBLNCE_VALUE_LOC = 0x804E2EFC
+.alias VOLBLNCE_VALUE_LOC_HI = 0x804E
+.alias VOLBLNCE_VALUE_LOC_LO = 0x2EFC
 
