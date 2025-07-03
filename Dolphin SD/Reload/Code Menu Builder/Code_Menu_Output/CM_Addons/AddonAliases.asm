035cb3caefc6003f04002bcd4cf5428a72f1bb4b
Aeon <efisiomii@gmail.com>
2025-07-03 23:03:42 +0200

    Importing Mario's costumes and UI to the build
    There are few specific instances where universal Mario textures aren't updated, gonna address those later

---
+++ Dolphin SD/Reload/Code Menu Builder/Code_Menu_Output/CM_Addons/AddonAliases.asm
@@ -0,0 +1,43 @@
+#################################################
+#[CM_Addons] Code Menu Addons Line Alias LOC Bank
+#################################################
+# Addon "Hitfalling Toggle v1.0.1 [Eon, mawwwk, QuickLava]" Lines
+# Line "Hitfalling Toggle" in "Hitfalling Toggle v1.0.1 [Eon, mawwwk, QuickLava]"
+.alias WIPMHTFL_ONOFF_LOC = 0x804E27B0
+.alias WIPMHTFL_ONOFF_LOC_HI = 0x804E
+.alias WIPMHTFL_ONOFF_LOC_LO = 0x27B0
+
+# Addon "Melee-style Camera Freeze on Game Finish v1.0.0 [Eon, QuickLava]" Lines
+# Line "Melee Game Finish Camera Freeze" in "Melee-style Camera Freeze on Game Finish v1.0.0 [Eon, QuickLava]"
+.alias MLEEFREZ_ONOFF_LOC = 0x804E27B4
+.alias MLEEFREZ_ONOFF_LOC_HI = 0x804E
+.alias MLEEFREZ_ONOFF_LOC_LO = 0x27B4
+
+# Addon "Raycast Debugger Toggle v1.0.1 [Eon, QuickLava]" Lines
+# Line "Raycast Display" in "Raycast Debugger Toggle v1.0.1 [Eon, QuickLava]"
+.alias RAYDEBUG_ONOFF_LOC = 0x804E27B8
+.alias RAYDEBUG_ONOFF_LOC_HI = 0x804E
+.alias RAYDEBUG_ONOFF_LOC_LO = 0x27B8
+
+# Addon "Parry Taunts v1.1.0" Lines
+# Line "Parry Taunts" in "Parry Taunts v1.1.0"
+.alias PRYTAUNT_ONOFF_LOC = 0x804E27BC
+.alias PRYTAUNT_ONOFF_LOC_HI = 0x804E
+.alias PRYTAUNT_ONOFF_LOC_LO = 0x27BC
+
+# Addon "Universal Walljumps v1.0.1 [Dantarion, QuickLava]" Lines
+# Line "Universal Walljump Toggle" in "Universal Walljumps v1.0.1 [Dantarion, QuickLava]"
+.alias UNVWLJMP_SETTING_LOC = 0x804E27C0
+.alias UNVWLJMP_SETTING_LOC_HI = 0x804E
+.alias UNVWLJMP_SETTING_LOC_LO = 0x27C0
+
+# Addon "Volume Balance Control v1.0.0 [QuickLava]" Lines
+# Working Memory (0x08 bytes) for "Volume Balance Control v1.0.0 [QuickLava]"
+.alias VOLBLNCE_WORKING_MEM_LOC = 0x804E27C4
+.alias VOLBLNCE_WORKING_MEM_LOC_HI = 0x804E
+.alias VOLBLNCE_WORKING_MEM_LOC_LO = 0x27C4
+# Line "Volume Ratio" in "Volume Balance Control v1.0.0 [QuickLava]"
+.alias VOLBLNCE_VALUE_LOC = 0x804E27CC
+.alias VOLBLNCE_VALUE_LOC_HI = 0x804E
+.alias VOLBLNCE_VALUE_LOC_LO = 0x27CC
+
